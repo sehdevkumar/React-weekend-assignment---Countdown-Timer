6efd78b06abf7458c01e91d016f1f77e128f873b
sehdev kumar <55868273+sehdevkumar@users.noreply.github.com>
2021-05-19 11:35:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import "./../styles/App.css";
 function App() {
     const [getCounter,setCounter] = useState(0);
     const [getValue,setValue] = useState(0);
+    const [get,set] = useState(0);
     const ref = useRef();
     useEffect(()=>{
       
@@ -27,14 +28,18 @@ function App() {
 
     const callerKey = (e)=>{
         if(e.key==="Enter"){
+
             setCounter(Math.floor(e.target.value));
         }
+    }
+    function updateME(e){
+        set(e.target.value);
     }
     return (
         <div className="wrapper">
             <div id="whole-center">
                 <h1>
-                    Reverse countdown for<input type="number" id="timeCount" onKeyDown={callerKey} /> sec.
+                    Reverse countdown for<input value={get} onChange={updateME}  type="number" id="timeCount" onKeyDown={callerKey} /> sec.
               </h1>
             </div>
             <div id="current-time">{getValue}</div>
